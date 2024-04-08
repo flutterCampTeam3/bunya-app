a9bfc896889742fc8599d546fd85275f8d4a8668
khaled_alghamdi <khaledtabah@gmail.com>
2024-04-09 02:05:45 +0300

    intro_pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bunya_app/pages/intro%20pages/first_intro.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: introPage()
+      
+      /*Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
+      */
     );
   }
 }
