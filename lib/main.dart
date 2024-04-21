83464b165f195131e1f4e18f8a12e08916408b33
Saod5557 <124809082+Saod5557@users.noreply.github.com>
2024-04-21 03:31:17 +0300

    fixUi

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:bunya_app/pages/Office%20pages/navBar%20page/navBarPage.dart';
 import 'package:bunya_app/pages/costumer%20pages/navBar%20page/navBarPage.dart';
 import 'package:bunya_app/pages/intro%20pages/disconnect_page.dart';
 import 'package:bunya_app/pages/intro%20pages/first_intro.dart';
-import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:get_it/get_it.dart';
 // import 'package:device_preview/device_preview.dart';import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
